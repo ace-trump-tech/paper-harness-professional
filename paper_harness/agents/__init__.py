28d657ab1f7f2db76804f7e7412a9b154af73329
摆烂仙君 <187950525+ace-trump-tech@users.noreply.github.com>
2026-08-27 16:00:57 +0800

    Sync non-CV power-grid literature workflow

--- paper_harness/agents/__init__.py
+++ paper_harness/agents/__init__.py
@@ -4,5 +4,6 @@ from .research import AdversarialCritic, EvidenceSynthesizer, ExperimentDesigner
 from .writing import DraftWriter
 from .visual import CompositionSupervisor, VisualCritic, VisualPlanner
 from .profiles import CompositionAgent, CVCombinationAgent, EvaluationSubAgent, GPUSubAgent, KnowledgeBaseAgent
+from .power_grid import PowerGridBoundaryAgent, PowerGridEvidenceAgent, PowerGridScopeAgent, PowerGridSourceAgent, PowerGridSynthesisAgent, PowerGridTaxonomyAgent
 
-__all__ = ["AuthorshipEditor", "ClaimAuditor", "CompositionSupervisor", "DraftWriter", "SimilarityChecker", "VisualCritic", "VisualPlanner", "AdversarialCritic", "EvidenceSynthesizer", "ExperimentDesigner", "HypothesisGenerator", "InnovationGenerator", "LiteratureScout", "CompositionAgent", "CVCombinationAgent", "EvaluationSubAgent", "GPUSubAgent", "KnowledgeBaseAgent"]
+__all__ = ["AuthorshipEditor", "ClaimAuditor", "CompositionSupervisor", "DraftWriter", "SimilarityChecker", "VisualCritic", "VisualPlanner", "AdversarialCritic", "EvidenceSynthesizer", "ExperimentDesigner", "HypothesisGenerator", "InnovationGenerator", "LiteratureScout", "CompositionAgent", "CVCombinationAgent", "EvaluationSubAgent", "GPUSubAgent", "KnowledgeBaseAgent", "PowerGridBoundaryAgent", "PowerGridEvidenceAgent", "PowerGridScopeAgent", "PowerGridSourceAgent", "PowerGridSynthesisAgent", "PowerGridTaxonomyAgent"]
